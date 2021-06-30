1eb740b5c9bc20130b5a2762ca42e6f4b8b18d8d
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-06-30 20:41:20 -0300

    reversion de la barra de busqueda

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 app.use(express.static(path.resolve(__dirname,"./public")));
 
-app.listen(3000,()=>{console.log("Corriendo en puerto 3000");});
+app.listen(process.env.PORT || 3000,()=>{console.log("Corriendo en puerto 3000");});
 
 app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"));
