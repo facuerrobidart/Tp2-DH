f2ef0f6f8e8535e5ad0be82d36f0d7ffab72a8b5
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-06-30 20:53:51 -0300

    de nuevo

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 app.use(express.static(path.resolve(__dirname,"./public")));
-
-app.listen(process.env.PORT || 3000,()=>{console.log("Corriendo en puerto 3000");});
+var port = process.env.PORT || 3000;
+app.listen(port,()=>{console.log("Corriendo en puerto", port);});
 
 app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"));
