2ddac00417fed5942126a796b87ee3da1bfa8e7e
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-07-02 13:16:36 -0300

    login

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 app.use(express.static(path.resolve(__dirname,"./public")));
-
+app.use(express.static(path.resolve(__dirname,"./views")));
 app.listen(process.env.PORT||3000,()=>{console.log("Corriendo en puerto");});
 
 app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/home.html"));
+});
+
+app.get("/login",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"));
 });
