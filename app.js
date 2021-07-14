11fb88b864f951cb080e8c811776a590a5f4c2b8
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-07-14 19:39:11 -0300

    modelo mvc

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const router = express().router;
 const path = require("path");
+
+app.set("view engine","ejs");
 app.use(express.static(path.resolve(__dirname,"./public")));
 app.use(express.static(path.resolve(__dirname,"./views")));
 app.listen(process.env.PORT||3000,()=>{console.log("Corriendo en puerto");});
